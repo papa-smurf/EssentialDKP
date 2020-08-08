0e77035c463a412cc55fc2cff9ea080f6a6c0bc2
lantisnt <43088589+lantisnt@users.noreply.github.com>
2020-08-08 16:20:38 +0200

    Bump version to v1.5.0

--- Core.lua
+++ Core.lua
@@ -84,8 +84,8 @@ core.EncounterList = {      -- Event IDs must be in the exact same order as core
 
 local BASE = 140000
 local MAJOR = 1
-local MINOR = 4
-local PATCH = 1
+local MINOR = 5
+local PATCH = 0
 
 core.MonDKPUI = {}        -- global storing entire Configuration UI to hide/show UI
 core.MonVersion = string.format("v%s.%s.%s", MAJOR, MINOR, PATCH)
